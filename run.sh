96ba0294cf9d9033a578d9e63f675968dfecc8ba
marina <besselfunction@mail.ru>
2020-02-13 16:03:12 +1100

    Add runner

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+FILE=./target/universal/stage/bin/iloveyou
+
+if [[ -f "$FILE" ]]; then
+    ./target/universal/stage/bin/iloveyou
+else
 sbt clean
 sbt stage
 
 cd target/universal/stage/
 ./bin/iloveyou
+
+fi
